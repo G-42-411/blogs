efac0c33dc593cc25cc52dc5c16e8d2d52e94799
KoKo <2792661835@qq.com>
2020-01-15 18:06:52 +0800

    Initial commit

---
+++ src/main/java/com/dreamland/BlogsApplication.java
@@ -0,0 +1,13 @@
+package com.dreamland;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogsApplication.class, args);
+    }
+
+}
