c1b5675668b861a5070acdb5f1c949c42422dcab
KoKo <2792661835@qq.com>
2020-01-17 17:05:03 +0800

    improve publish function

--- src/main/java/com/dreamland/controller/publishController.java
+++ src/main/java/com/dreamland/controller/publishController.java
@@ -1,12 +1,82 @@
 package com.dreamland.controller;
 
+import com.dreamland.mapper.QuestionMapper;
+import com.dreamland.mapper.UserMapper;
+import com.dreamland.pojo.Question;
+import com.dreamland.pojo.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class publishController {
-    @RequestMapping("/publish")
-    public String publish(){
+
+    @Autowired
+    QuestionMapper questionMapper;
+    @Autowired
+    UserMapper userMapper;
+
+    @GetMapping("/publish")
+    public String publish() {
         return "publish";
+    }
+
+    @PostMapping("/publish")
+    public String doPublish(
+            @RequestParam("title") String title,
+            @RequestParam("description") String description,
+            @RequestParam("tag") String tag,
+            HttpServletRequest request,
+            Model model) {
+        Question question = new Question();
+        User user = null;
+
+        model.addAttribute("title",title);
+        model.addAttribute("description",description);
+        model.addAttribute("tag",tag);
+
+        if(title == null || title == ""){
+            model.addAttribute("msg","标题不能为空");
+            return "/publish";
+        }
+        if(description == null || description == ""){
+            model.addAttribute("msg","内容不能为空");
+            return "/publish";
+        }
+        if(tag == null || tag == ""){
+            model.addAttribute("msg","标签不能为空");
+            return "/publish";
+        }
+
+
+        Cookie[] cookies = request.getCookies();
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                if (cookie.getName().equals("token")) {
+                    String token = cookie.getValue();
+                    user = userMapper.findUserByToken(token);
+                    if (user == null) {
+                        model.addAttribute("msg", "用户未登录");
+                        return "publish";
+                    }
+                    break;
+                }
+            }
+        }
+        question.setTitle(title);
+        question.setDescription(description);
+        question.setTag(tag);
+        question.setCreator(Integer.parseInt(user.getAccountId()));
+        question.setGmtCreate(System.currentTimeMillis());
+        question.setGmtModified(question.getGmtCreate());
+
+        questionMapper.create(question);
+        return "redirect:/";
     }
 }
