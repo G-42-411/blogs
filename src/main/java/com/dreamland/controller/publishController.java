10e9e1f31ca5d60ebde48adcf2101cbd1db8d7ce
KoKo <2792661835@qq.com>
2020-01-17 14:42:28 +0800

    add publish.html

---
+++ src/main/java/com/dreamland/controller/publishController.java
@@ -0,0 +1,12 @@
+package com.dreamland.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class publishController {
+    @RequestMapping("/publish")
+    public String publish(){
+        return "publish";
+    }
+}
